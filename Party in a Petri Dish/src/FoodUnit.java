a64ae74cba80795641176c951e21033f98561e02
Georgiana <georgiana.marti.gg@gmail.com>
2023-11-04 21:19:37 +0200

    food unit class

---
+++ Party in a Petri Dish/src/FoodUnit.java
@@ -0,0 +1,33 @@
+public class FoodUnit {
+    private int x, y;  // Food unit's position
+    private boolean isEaten = false;
+
+
+    public FoodUnit(int x, int y, int tFull) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isEaten() {
+        return isEaten;
+    }
+
+    public void markAsEaten() {
+        isEaten = true;
+    }
+
+
+}
