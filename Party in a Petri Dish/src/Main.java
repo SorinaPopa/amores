d44b52d3e8c6798238407aab564008bcbbaae30f
sori <sorinapopaan@gmail.com>
2023-11-26 14:01:07 +0200

    Added a Spawn Food Units to the PetriDish

--- Party in a Petri Dish/src/Main.java
+++ Party in a Petri Dish/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //set how big the petri dish is
+        PetriDish petriDish = new PetriDish(new int[]{30, 30});
+
         //x and y to be random positions on the petri dish
-        Bacteria mara=new Bacteria("sexual",3,5);
+        Bacteria mara = new Bacteria("sexual", 3, 5);
         System.out.println(mara.IsAsexual());
     }
 }
--- Party in a Petri Dish/src/PetriDish.java
+++ Party in a Petri Dish/src/PetriDish.java
@@ -1,18 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 
 public class PetriDish {
     private int[] dimension;
-    private List<Bacteria> bacterias=new ArrayList<>();
-    //private FoodUnit[] foodUnits=new ArrayList<>();
-    public PetriDish(int[] dimension){
-        this.dimension=dimension;
+    private int[][] matrix;
+    private List<Bacteria> bacteriaList = new ArrayList<>();
+    private List<FoodUnit> foodUnitList = new ArrayList<>();
 
+    public PetriDish(int[] dimension) {
+        this.dimension = dimension;
+        this.matrix = new int[dimension[0]][dimension[1]];
     }
 
-    public void Populate(Object obj){
-        if(obj instanceof Bacteria){
-            bacterias.add((Bacteria) obj);
+    public void update() {
+        // Update the state of the Petri Dish over time
+        // This can include spawning new food units, updating bacteria positions, etc.
+
+        // For simplicity, let's spawn a new food unit randomly
+        spawnFoodUnit();
+    }
+
+    private void spawnFoodUnit() {
+        Random random = new Random();
+        int x = random.nextInt(dimension[0]);
+        int y = random.nextInt(dimension[1]);
+
+        FoodUnit newFoodUnit = new FoodUnit(x, y,5);
+        foodUnitList.add(newFoodUnit);
+
+        System.out.println("New food unit spawned at (" + x + ", " + y + ")");
+    }
+
+    public List<FoodUnit> getFoodUnits() {
+        return foodUnitList;
+    }
+
+    public void Populate(Object obj) {
+        if (obj instanceof Bacteria) {
+            bacteriaList.add((Bacteria) obj);
         }
         /*else if (obj instanceof FoodUnit) {
             foodUnits.add((FoodUnit) obj);
